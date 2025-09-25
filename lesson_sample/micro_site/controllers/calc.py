a5fae77a841fd3a53c7ee228aa84c932734a4019
ktaiga1016 <kmm2459301@stu.o-hara.ac.jp>
2025-09-25 11:13:12 +0900

    commit

--- lesson_sample/micro_site/controllers/calc.py
+++ lesson_sample/micro_site/controllers/calc.py
@@ -3,19 +3,19 @@ from utils import parse_post, render_template
 def calc(environ):
     result = None
     method = environ["REQUEST_METHOD"]
-    a = b = ""
+    set_first_value = set_second_value = ""
     if method == "POST":
         data = parse_post(environ)
-        a = data.get("a", [""])[0]
-        b = data.get("b", [""])[0]
+        set_first_value = data.get("a", [""])[0]
+        set_second_value = data.get("b", [""])[0]
         try:
-            result = int(a) + int(b)
+            result = int(set_first_value) + int(set_second_value)
         except Exception:
             result = "Invalid input"
     result_block = f"<p>答え: {result}</p>" if result is not None else ""
     return render_template(
         "boundaries/calc.html",
-        a=a,
-        b=b,
+        set_first_value=set_first_value,
+        set_second_value=set_second_value,
         result_block=result_block
     )
